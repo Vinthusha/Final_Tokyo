f273e981606fd52e010998d2c8dfb1690493fd23
Jathumithran145 <jazzjack145@gmail.com>
2020-07-17 10:24:24 +0530

    first commit

--- src/test/java/mastertest/EmployeeNV/Designation.java
+++ src/test/java/mastertest/EmployeeNV/Designation.java
@@ -1,6 +1,7 @@
 package mastertest.EmployeeNV;
 
 import controllers.MethodBase;
+import controllers.PageBase;
 import controllers.TestBase;
 import org.testng.annotations.Test;
 
@@ -8,12 +9,18 @@ public class Designation extends TestBase {
 
     @Test(groups = {"Smoke"}, priority = 1)
     public void DesignationTest() {
-        MethodBase.click_ByXpath("//*[@id=\"root\"]/div/section/section/main/div/div[1]/div[1]/a[1]/div/div");
-        MethodBase.click_ByXpath("//*[@id=\"root\"]/div/section/section/main/div/div[1]/div[1]/a[1]/div/div");
-        MethodBase.click_ByXpath("//*[@id=\"root\"]/div/section/section/header/ul/li[15]/a");
-        MethodBase.click_ByXpath("//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/div[1]/div/div[2]/button");
+        PageBase.staticWait(5);
+        MethodBase.click_ByXpath("//*[@id=\"root\"]/div/section/section/main/div/div[1]/div[1]/a[1]/div/div");// Master
+        PageBase.staticWait(5);
+        MethodBase.click_ByXpath("//*[@id=\"root\"]/div/section/section/header/ul/li[15]/a");// Employee NV
+        PageBase.staticWait(5);
+        MethodBase.click_ByXpath("//*[@id=\"root\"]/div/section/section/main/div/div[1]/div[1]");// Designation Button
+        PageBase.staticWait(5);
+        MethodBase.click_ByXpath("//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/div[1]/div/div[2]/button");// Add Designation
+        PageBase.staticWait(5);
         MethodBase.setText_ByXpath("//*[@id=\"designation_name\"]", "add employee");
         MethodBase.setText_ByXpath("//*[@id=\"designation_description\"]", "employee");
+        PageBase.staticWait(5);
         MethodBase.click_ByXpath("/html/body/div[3]/div/div[2]/div/div[2]/div[3]/button[2]");
     }
 }
--- src/test/java/mastertest/EmployeeNV/Employee.java
+++ src/test/java/mastertest/EmployeeNV/Employee.java
@@ -13,15 +13,15 @@ public class Employee extends TestBase {
 
     @Test(groups = {"Smoke"}, priority = 1)
     public void employeeSmokeTest() throws Exception {
-        softAssert = new SoftAssert();
+       // softAssert = new SoftAssert();
         PageBase.staticWait(4);
-        MethodBase.click_ByXpath("//*[@id=\"root\"]/div/section/section/main/div/div[1]/div[1]/a[1]/div/div");//click master
-        MethodBase.click_ByXpath("//*[@id=\"root\"]/div/section/section/header/ul/li[15]/a");// click employee NV
-        MethodBase.click_ByXpath("//*[@id=\"root\"]/div/section/section/main/div/div[1]/div[3]/div[1]");//click employee panel
+        MethodBase.click_ByXpath("//h1[contains(text(),'Master')]");//click master
+        MethodBase.click_ByXpath("//*[@id=\"root\"]/div/section/section/header/ul/li[15]");// click employee NV
+        MethodBase.click_ByXpath("//*[@id=\"root\"]/div/section/section/main/div/div[1]/div[3]");//click employee panel
         MethodBase.click_ByXpath("//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/div/div[1]/div/div[2]/div[2]/div/button");//click add employee
-        MethodBase.click_ByXpath("/html/body/div[9]/div/div[2]/div/div[2]/div[3]/button[2]");
-        MethodBase.click_ByXpath("/html/body/div[9]/div/div[2]/div/div[2]/div[3]/button[2]");//click save
-        softAssert.assertEquals(MethodBase.get_Text("/html/body/div[9]/div/div[2]/div/div[2]/div[3]/button[2]"),"Name can't be empty","there are not equal");
+
+       // MethodBase.click_ByXpath("/html/body/div[9]/div/div[2]/div/div[2]/div[3]/button[2]");//click save
+       // softAssert.assertEquals(MethodBase.get_Text("/html/body/div[9]/div/div[2]/div/div[2]/div[3]/button[2]"),"Name can't be empty","there are not equal");
         MethodBase.setText_ByID("first_name","vinthusha");//set first name
         MethodBase.setText_ByID("last_name","Loganathan");// set laste name
         MethodBase.click_ByXpath("//*[@id=\"plant\"]/div/div"); // click plant
@@ -30,11 +30,12 @@ public class Employee extends TestBase {
         MethodBase.click_ByXpath("//*[@id=\"desigination\"]/div/div"); // click designation
         PageBase.staticWait(02);
         MethodBase.hitEnter();
-        MethodBase.setText_ByID("address","kandy");// set address
+        MethodBase.setText_ByID("address","kandyyyyyyy");// set address
         MethodBase.setText_ByID("phoneno","0765689631");// set phone number
         MethodBase.setText_ByID("email","vinthusha96@gmail.com");
-        MethodBase.click_ByXpath("/html/body/div[10]/div/div[2]/div/div[2]/div[3]/button[2]");//click save
-        softAssert.assertAll();
+
+        MethodBase.click_ByXpath("/html/body/div[9]/div/div[2]/div/div[2]/div[3]/button[2]");//click save
+       // softAssert.assertAll();
 
 
     }
